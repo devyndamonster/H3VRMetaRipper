09f7e1678dc4350597d47038478da5222dd97caf
Devyn Myers <devyndamonster@hotmail.com>
2022-01-12 21:12:50 -0500

    Add project files.

---
+++ MetaRipperPlugin.cs
@@ -0,0 +1,144 @@
+﻿using BepInEx;
+using FistVR;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using Valve.Newtonsoft;
+using Valve.Newtonsoft.Json;
+
+namespace MetaRipper {
+
+    [BepInPlugin("devyndamonster.metaripper", "Meta Ripper", "1.0.0")]
+    public class MetaRipperPlugin : BaseUnityPlugin
+    {
+        private void Start()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Loaded Meta Ripper");
+
+            StartCoroutine(WaitForMetadataComplete());
+        }
+
+        private IEnumerator WaitForMetadataComplete()
+        {
+            Logger.LogInfo($"Waiting for meta data to be ready");
+
+            while (MagazinePatcher.PatcherStatus.PatcherProgress < 1)
+            {
+                yield return null;
+            }
+
+            Logger.LogInfo($"Metadata is ready");
+
+            RipMetaData();
+        }
+
+
+        private string SetupOutputDirectory()
+        {
+            string outputFilePath = Application.dataPath.Replace("/h3vr_Data", "/MetaRipper");
+
+            if (!Directory.Exists(outputFilePath))
+            {
+                Directory.CreateDirectory(outputFilePath);
+            }
+
+            return outputFilePath;
+        }
+
+
+        private void RipMetaData()
+        {
+            Logger.LogInfo($"Ripping metadata");
+
+            string outputFilePath = SetupOutputDirectory();
+
+            CreateMetaCSV(outputFilePath);
+            CreateMetaJSON(outputFilePath);
+        }
+
+
+        private void CreateMetaCSV(string path)
+        {
+            try
+            {
+                if (File.Exists(path + "/ObjectData.csv"))
+                {
+                    File.Delete(path + "/ObjectData.csv");
+                }
+
+                // Create a new file     
+                using (StreamWriter sw = File.CreateText(path + "/ObjectData.csv"))
+                {
+                    sw.WriteLine("ObjectID,Category,Era,Set,Country of Origin,Attachment Feature,Firearm Action,Firearm Feed Option,Firing Modes,Firearm Mounts,Attachment Mount,Round Power,Size,Melee Handedness,Melee Style,Powerup Type,Thrown Damage Type,Thrown Type");
+                    foreach (FVRObject obj in IM.OD.Values)
+                    {
+                        sw.WriteLine(
+                            obj.ItemID.Replace(",", ".") + "," +
+                            obj.Category + "," +
+                            obj.TagEra + "," +
+                            obj.TagSet + "," +
+                            obj.TagFirearmCountryOfOrigin + "," +
+                            obj.TagAttachmentFeature + "," +
+                            obj.TagFirearmAction + "," +
+                            string.Join("+", obj.TagFirearmFeedOption.Select(o => o.ToString()).ToArray()) + "," +
+                            string.Join("+", obj.TagFirearmFiringModes.Select(o => o.ToString()).ToArray()) + "," +
+                            string.Join("+", obj.TagFirearmMounts.Select(o => o.ToString()).ToArray()) + "," +
+                            obj.TagAttachmentMount + "," +
+                            obj.TagFirearmRoundPower + "," +
+                            obj.TagFirearmSize + "," +
+                            obj.TagMeleeHandedness + "," +
+                            obj.TagMeleeStyle + "," +
+                            obj.TagPowerupType + "," +
+                            obj.TagThrownDamageType + "," +
+                            obj.TagThrownType);
+                    }
+                    sw.Close();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.ToString());
+            }
+        }
+
+
+        private void CreateMetaJSON(string path)
+        {
+            try
+            {
+                if (File.Exists(path + "/ObjectData.json"))
+                {
+                    File.Delete(path + "/ObjectData.json");
+                }
+
+                // Create a new file     
+                using (StreamWriter sw = File.CreateText(path + "/ObjectData.json"))
+                {
+                    List<MetadataEntry> entries = new List<MetadataEntry>();
+
+                    foreach(FVRObject fvrObject in IM.OD.Values)
+                    {
+                        entries.Add(new MetadataEntry(fvrObject));
+                    }
+    
+                    string json = JsonConvert.SerializeObject(entries);
+
+                    sw.WriteLine(json);
+                    sw.Close();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.ToString());
+            }
+        }
+
+    }
+
+}
---
+++ MetadataEntry.cs
@@ -0,0 +1,104 @@
+﻿using FistVR;
+using MagazinePatcher;
+using OtherLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MetaRipper
+{
+    public class MetadataEntry
+    {
+
+        public string ObjectID;
+        public string Category;
+        public string Era;
+        public string Set;
+        public string CountryOfOrigin;
+        public int FirstYear;
+        public string FirearmSize;
+        public string FirearmAction;
+        public string FirearmRoundPower;
+        public string AttachmentFeature;
+        public string AttachmentMount;
+        public string MeleeHandedness;
+        public string MeleeStyle;
+        public string PowerupType;
+        public string ThrownDamageType;
+        public string ThrownType;
+
+        public List<string> FirearmFeedOptions = new List<string>();
+        public List<string> FirearmFiringModes = new List<string>();
+        public List<string> FirearmMounts = new List<string>();
+        public List<string> ModTags = new List<string>();
+
+        public int MagazineType;
+        public int ClipType;
+        public int RoundType;
+        public int Capacity;
+        public int MinCapacityRelated;
+        public int MaxCapacityRelated;
+
+        public bool DoesUseSpeedLoader;
+        public bool UsesRoundType;
+        public bool IsModContent;
+
+        public bool HasSpawnerEntry;
+        public string EntryPath;
+        public bool IsDisplayedInMainEntry;
+
+        public MetadataEntry(FVRObject fvrObject)
+        {
+            ObjectID = fvrObject.ItemID;
+            Category = fvrObject.Category.ToString();
+            Era = fvrObject.TagEra.ToString();
+            Set = fvrObject.TagSet.ToString();
+            CountryOfOrigin = fvrObject.TagFirearmCountryOfOrigin.ToString();
+            FirstYear = fvrObject.TagFirearmFirstYear;
+            FirearmSize = fvrObject.TagFirearmSize.ToString();
+            FirearmAction = fvrObject.TagFirearmAction.ToString();
+            FirearmRoundPower = fvrObject.TagFirearmRoundPower.ToString();
+            AttachmentFeature = fvrObject.TagAttachmentFeature.ToString();
+            AttachmentMount = fvrObject.TagAttachmentMount.ToString();
+            MeleeHandedness = fvrObject.TagMeleeHandedness.ToString();
+            MeleeStyle = fvrObject.TagMeleeStyle.ToString();
+            PowerupType = fvrObject.TagPowerupType.ToString();
+            ThrownDamageType = fvrObject.TagThrownDamageType.ToString();
+            ThrownType = fvrObject.TagThrownType.ToString();
+
+            FirearmFeedOptions = fvrObject.TagFirearmFeedOption.Select(x => x.ToString()).ToList();
+            FirearmFiringModes = fvrObject.TagFirearmFiringModes.Select(x => x.ToString()).ToList();
+            FirearmMounts = fvrObject.TagFirearmMounts.Select(x => x.ToString()).ToList();
+
+            MagazineType = (int)fvrObject.MagazineType;
+            ClipType = (int)fvrObject.ClipType;
+            RoundType = (int)fvrObject.RoundType;
+            Capacity = fvrObject.MagazineCapacity;
+            MinCapacityRelated = fvrObject.MinCapacityRelated;
+            MaxCapacityRelated = fvrObject.MaxCapacityRelated;
+
+            UsesRoundType = fvrObject.UsesRoundTypeFlag;
+            IsModContent = fvrObject.IsModContent;
+
+            if(fvrObject.Category == FVRObject.ObjectCategory.Firearm)
+            {
+                MagazineCacheEntry entry;
+                if(CompatibleMagazineCache.Instance.Entries.TryGetValue(fvrObject.ItemID, out entry))
+                {
+                    DoesUseSpeedLoader = entry.DoesUseSpeedloader;
+                }
+            }
+
+
+            ItemSpawnerEntry spawnerEntry;
+            if(OtherLoader.OtherLoader.SpawnerEntriesByID.TryGetValue(fvrObject.ItemID, out spawnerEntry))
+            {
+                HasSpawnerEntry = true;
+                EntryPath = spawnerEntry.EntryPath;
+                IsDisplayedInMainEntry = spawnerEntry.IsDisplayedInMainEntry;
+            }
+        }
+
+    }
+}
